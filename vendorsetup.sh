3adcc6ff2da745284640d64f57f9714b9cd7a976
rdx420 <padraramesh420@gmail.com>
2022-02-05 00:19:52 +0530

    violet: Update vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,8 @@
+git clone https://github.com/ArrowOS/android_external_ant-wireless_antradio-library.git -b arrow-12.0 external/ant-wireless/antradio-library
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-19.0 packages/resources/devicesettings
+git clone https://github.com/LineageOS/android_device_qcom_sepolicy_vndr.git -b lineage-19.0 device/qcom/sepolicy_vndr
+git clone https://github.com/LineageOS/android_device_lineage_sepolicy.git -b lineage-19.0 device/lineage/sepolicy
+git clone https://github.com/LineageOS/android_device_qcom_sepolicy.git -b lineage-19.0 device/qcom/sepolicy
 rm -rf hardware/qcom-caf/sm8150/audio
 rm -rf hardware/qcom-caf/sm8150/media
 rm -rf hardware/qcom-caf/sm8150/display
