036719ffb48b163e2aada5359ef8c21c85956414
twistloop <masteratman@gmail.com>
2022-02-11 20:31:59 +0530

    violet: overlay-lineage: Enable config_deviceLightCapabilities

--- device.mk
+++ device.mk
@@ -19,7 +19,8 @@ $(call inherit-product, vendor/xiaomi/violet/violet-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-awaken
+    $(LOCAL_PATH)/overlay-awaken \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
