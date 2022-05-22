5b51d17d5fefcdd39a4227b91c42e46ecec6bd70
rdx420 <padraramesh420@gmail.com>
2022-05-22 09:23:59 +0530

    violet: Switch to Inline Xcalibur Kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/rdx420/S_vendor_xiaomi_violet.git -b twelve vendor/xiaomi/violet
-git clone https://github.com/rdx420/MsM-4.14-RyZeN-.git -b R kernel/xiaomi/violet
+git clone https://github.com/SuperiorOS-Devices/kernel_xiaomi_violet.git -b twelve kernel/xiaomi/violet
 git clone https://gitlab.com/rdx420/anxcamera.git -b Base vendor/ANXCamera
 rm -rf hardware/qcom-caf/sm8150/audio
 rm -rf hardware/qcom-caf/sm8150/media
