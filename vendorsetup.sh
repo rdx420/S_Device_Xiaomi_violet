8e396d6d4547ca2b0806f2877786e85d5c5135c1
Twel12 <shivansh.aeyron@gmail.com>
2022-05-22 09:28:16 +0530

    violet: Use proton clang to compile kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/rdx420/S_vendor_xiaomi_violet.git -b twelve vendor/xiaomi/violet
 git clone https://github.com/SuperiorOS-Devices/kernel_xiaomi_violet.git -b twelve kernel/xiaomi/violet
+git clone https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton
 git clone https://gitlab.com/rdx420/anxcamera.git -b Base vendor/ANXCamera
 rm -rf hardware/qcom-caf/sm8150/audio
 rm -rf hardware/qcom-caf/sm8150/media
