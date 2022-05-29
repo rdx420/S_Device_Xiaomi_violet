be0c8afa8b30cd6c3e17357f388c3d73d247bbf9
rdx420 <padraramesh420@gmail.com>
2022-05-29 08:22:33 +0530

    violet: Add prebuilt kernel Headers

--- device.mk
+++ device.mk
@@ -41,6 +41,9 @@ PRODUCT_PACKAGES += \
    Clock \
    N_Messaging 
 
+# Headers
+PRODUCT_VENDOR_KERNEL_HEADERS := device/xiaomi/violet/prebuilt/kernel-headers
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
