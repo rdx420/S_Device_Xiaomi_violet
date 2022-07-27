cabe8d5e4352520fdc669d130ef0f12b0f4c35c4
rdx420 <padraramesh420@gmail.com>
2022-07-27 11:10:43 +0530

    violet: Clone Upstream Hals

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,9 @@ git clone https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/
 git clone https://gitlab.com/Joker-V2/android_vendor_ANXCamera.git -b twelve vendor/ANXCamera
 rm -rf packages/resources/devicesettings
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-19.1 packages/resources/devicesettings
+rm -rf hardware/qcom-caf/sm8150/audio
+rm -rf hardware/qcom-caf/sm8150/display
+rm -rf hardware/qcom-caf/sm8150/media
+git clone https://github.com/SuperiorOS/android_hardware_qcom_media.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/media
+git clone https://github.com/SuperiorOS/android_hardware_qcom_audio.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/audio
+git clone https://github.com/SuperiorOS/android_hardware_qcom_display.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/display
